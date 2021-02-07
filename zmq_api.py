28958e8ce277dcbbd14520482900db7b745c2e4d
jdunn-git <joshuadunncs@gmail.com>
2021-02-07 15:42:22 -0500

    error fixes

--- zmq_api.py
+++ zmq_api.py
@@ -7,30 +7,32 @@ context = zmq.Context()
 pub_dict = dict()
 sub_dict = dict()
 
-	pub_socket = context.socket(zmq.PUB)
+pub_socket = context.socket(zmq.PUB)
 sub_socket = context.socket(zmq.SUB)
 
 
 def register_pub(ip, topic):
-	pub_socket.bind("tcp://%s:5555" % ip)
+	pub_socket.bind("tcp://%s:5556" % ip)
 	pub_dict[topic] = pub_socket
 
 
 def register_sub(ip, topic_filter):
-	sub_socket.bind("tcp://10.0.0.1:5556")
+	sub_socket.connect("tcp://%i:5556" % ip)
 	sub_socket.setsockopt_string(zmq.SUBSCRIBE, topic_filter)
 	sub_dict[topic_filter] = sub_socket
-	string = sub_socket.recv_string()
-	print("Received: %s" % string)
 
 
 def publish(topic, value):
-	pub_socket.send_string(value)
-	print("Sending Data")
+	if pub_dict.get(topic) != None:
+		pub_socket.send_string(value)
+		print("Sending Data")
+
 
 def listen(topic):
-	string = sub_dict.get(topic).recv_string()
-	return string
+	if sub_dict.get(topic) != None:
+		sub_socket = sub_dict.get(topic)
+		string = sub_dict.get(topic).recv_string()
+		return string
 
 
 #def notify(topic, value):
