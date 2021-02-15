c4b94f3ead08dadfb970ae9721d9dc4c4e1ae59d
terrence-d-williams <terrence.d.williams@vanderbilt.edu>
2021-02-15 11:47:48 -0500

    Updating mr_topology for assignment

--- assignment_executor.py
+++ assignment_executor.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+import threading
 
 from signal import SIGINT
 import time
@@ -20,3 +21,47 @@ from mininet.util import pmonitor
 from mr_topology import MR_Topo
 
 from mininet.node import OVSController
+
+
+def parse_args():
+    # parse the command line
+    parser = argparse.ArgumentParser()
+
+    # add optional arguments
+    parser.add_argument('topics', metavar="tops", type=str, nargs="+")
+    parser.add_argument("-s", "--subscribers", type=int, default=10, help="Number of subscribers, default 10, minimum is number of topics")
+    parser.add_argument("-p", "--publishers", type=int, default=2, help="Number of publishers, default 2, minimum is number of topics")
+    parser.add_argument("-r", "--racks", type=int, default=1, help="Number of racks, choices 1, 2 or 3")
+    parser.add_argument("-f", "--folder", type=str, default="NEW_REPORT")
+
+    parser.add_argument("-b", "--broker_mode", default=False, action="store_true")
+
+    # parse the args
+    args = parser.parse_args ()
+
+    args.subs = max(args.subscribers, len(args.topics))
+    args.pubs = max(args.publishers, len(args.topics))
+    return args
+
+
+def main():
+    parse_args = parse_args()
+
+    # Create Topology
+    print("Creating Topology")
+    topology = MR_Topo(racks = parse_args.racks,
+                       publishers = parse_args.publishers,
+                       subscribers = parse_args.subscribers,
+                       broker_mode = parse_args.broker_mode )
+
+    # Create Network
+    print("Creating network")
+    network = Mininet(topology, link=TCLink, controller = OVSController)
+
+    # activate the network
+    print("Activating network")
+    network.start()
+
+    # debugging purposes
+    print("Dumping host connections")
+    dumpNodeConnections(network.hosts)
--- mr_topology.py
+++ mr_topology.py
@@ -24,14 +24,17 @@ class MR_Topo (Topo):
     # rack, the map nodes on 2nd rack and reduce nodes on the third rack. Number of
     # switches equals the number of racks.
 
-    def build (self, Racks=1, M=10, R=3):
-        print("Topology: Racks = ", Racks, ", M = ", M, ", R = ", R)
+    def build (self, racks=1, publishers=10, subscribers=3, broker_mode=False):
+        print(f"Topology: Racks = {racks},
+                publishers = {publishers},
+                subsribers = {subscribers},
+                broker_mode = {broker_mode}")
         self.mr_switches = []
         self.mr_hosts = []
         # Python's range(N) generates 0..N-1
-        for r in range (Racks):
+        for r in range(racks):
             # a switch per rack.
-            switch = self.addSwitch ('s{}'.format(r+1))
+            switch = self.addSwitch (f's{r+1}')
             print("Added switch", switch)
             self.mr_switches.append (switch)
             if (r > 0):
@@ -42,28 +45,45 @@ class MR_Topo (Topo):
         host_index = 0
         switch_index = 0
         # Now add the master node (host master) on rack 1, i.e., switch 1
+
+        if broker_mode:
+            # Now add the master node (host master) on rack 1, i.e., switch 1
+            host = self.addHost (f'BROKERh{host_index+1}s{switch_index+1}')
+            print("Added master host", host)
+            self.addLink (host, self.mr_switches[switch_index], delay='1ms')  # zero based indexing
+            print(f"Added link between {host} and switch {self.mr_switches[switch_index]}")
+            self.mr_hosts.append (host)
+            host_index += 1
+
+
         host = self.addHost ('h{}s{}'.format (host_index+1, switch_index+1))
         print("Added master host", host)
         self.addLink (host, self.mr_switches[switch_index], delay='1ms')  # zero based indexing
         print("Added link between ", host, " and switch ", self.mr_switches[switch_index])
         self.mr_hosts.append (host)
 
-        # Now add the M map nodes to the next available rack
-        switch_index = (switch_index + 1) % Racks
-        for h in range (M):
-            host_index = host_index +1
-            host = self.addHost('h{}s{}'.format (host_index+1, switch_index+1))
-            print("Added next map host", host)
-            self.addLink(host, self.mr_switches[switch_index], delay='1ms')
-            print("Added link between ", host, " and switch ", self.mr_switches[switch_index])
-            self.mr_hosts.append (host)
+        # Now add the SUB nodes
+        switch_index = 1
+        for h in range (subscribers):
+            SWITCH = (switch_index % racks) - 1;
+            host = self.addHost('SUBh{}s{}'.format (host_index+1, SWITCH+1))
+            print("Added next SUB host", host)
+            self.addLink(host, self.mr_switches[SWITCH], delay='1ms')
+            print(f"Added link between {host} and switch {self.mr_switches[SWITCH]}")
+            self.mr_hosts.append(host)
 
-        # Now add the R reduce nodes to the next available rack
-        switch_index = (switch_index + 1) % Racks
-        for h in range (R):
-            host_index = host_index +1
-            host = self.addHost('h{}s{}'.format (host_index+1, switch_index+1))
-            print("Added next reduce host", host)
-            self.addLink(host, self.mr_switches[switch_index], delay='1ms')
-            print("Added link between ", host, " and switch ", self.mr_switches[switch_index])
-            self.mr_hosts.append (host)
+            switch_index += 1
+            host_index += 1
+
+        # Now add the PUB nodes
+        switch_index = 1
+        for h in range (publishers):
+            SWITCH = (switch_index % racks) - 1;
+            host = self.addHost(f"PUBh{host_index+1}s{SWITCH+1}")
+            print(f"Added next PUB host {host}")
+            self.addLink(host, self.mr_switches[SWITCH], delay='1ms')
+            print(f"Added link between {host} and switch {self.mr_switches[SWITCH]}")
+            self.mr_hosts.append(host)
+
+            switch_index += 1
+            host_index += 1
